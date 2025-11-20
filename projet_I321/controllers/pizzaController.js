380fe735c658bbef8fcf14bab28465a911093b90
Kilian Testard <pc47xiy@eduvaud.ch>
2025-11-20 11:57:38 +0100

    feat: explicit 200 status

--- projet_I321/controllers/pizzaController.js
+++ projet_I321/controllers/pizzaController.js
@@ -3,7 +3,7 @@ const PizzaModel = require('../entities/pizzas.js');
 exports.getPizzas = async (req, res) => {
     try {
         const pizzas = await PizzaModel.getAllPizzas();
-        res.json(pizzas);
+        res.status(200).json(pizzas);
     } catch (err) {
         console.error(err);
         res.status(500).json({ error: "Unexpected server error." });
