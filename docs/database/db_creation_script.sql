6b2e77b0dad11cd3a9745de5ad6be0952b50d8d3
Kilian Testard <pc47xiy@eduvaud.ch>
2025-11-20 11:51:51 +0100

    feat: db modifications

--- docs/database/db_creation_script.sql
+++ docs/database/db_creation_script.sql
@@ -8,7 +8,6 @@ CREATE TABLE pizzas (
     name VARCHAR(50) NOT NULL,
     imageURL VARCHAR(500),
     price DECIMAL(6,2) NOT NULL,
-    pizza_of_the_day BOOL NOT NULL,
     created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
@@ -26,4 +25,14 @@ CREATE TABLE pizza_ingredients (
     PRIMARY KEY (pizza_id, ingredient_id),
     FOREIGN KEY (pizza_id) REFERENCES pizzas(id) ON DELETE CASCADE,
     FOREIGN KEY (ingredient_id) REFERENCES ingredients(id) ON DELETE CASCADE
+);
+
+DROP TABLE IF EXISTS promotion;
+CREATE TABLE promotion (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+	 pizza_id INT NOT NULL,
+    percentage INT NOT NULL,
+    starting_date DATE NOT NULL,
+    end_date DATE NOT NULL,
+    FOREIGN KEY (pizza_id) REFERENCES pizzas(id) ON DELETE CASCADE
 );
