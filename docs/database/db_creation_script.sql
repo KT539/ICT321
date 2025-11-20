bb75b06721866b253836394a29ade5751ff521f1
Niels Delafontaine <pa76wlu@eduvaud.ch>
2025-11-20 10:44:23 +0100

    Fix: Rename doc to docs to fit the name decided by the teacher

---
+++ docs/database/db_creation_script.sql
@@ -0,0 +1,29 @@
+DROP DATABASE IF EXISTS projet_foodtruck;
+CREATE DATABASE projet_foodtruck;
+USE projet_foodtruck;
+
+DROP TABLE IF EXISTS pizzas;
+CREATE TABLE pizzas (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    imageURL VARCHAR(500),
+    price DECIMAL(6,2) NOT NULL,
+    pizza_of_the_day BOOL NOT NULL,
+    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+DROP TABLE IF EXISTS ingredients;
+CREATE TABLE ingredients (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL UNIQUE
+);
+
+DROP TABLE IF EXISTS pizza_ingredients;
+CREATE TABLE pizza_ingredients (
+    pizza_id INT NOT NULL,
+    ingredient_id INT NOT NULL,
+    PRIMARY KEY (pizza_id, ingredient_id),
+    FOREIGN KEY (pizza_id) REFERENCES pizzas(id) ON DELETE CASCADE,
+    FOREIGN KEY (ingredient_id) REFERENCES ingredients(id) ON DELETE CASCADE
+);
