f5da33df3054184de85fbd7511f0a4cc5f1ec8a3
hev0s <niels.delafontaine@eduvaud.ch>
2025-11-27 10:51:29 +0100

    Remove comment from ingredients.js
    Removed comment.

--- projet_I321/entities/ingredients.js
+++ projet_I321/entities/ingredients.js
@@ -1,6 +1,6 @@
 const db = require('../config/database.js'); // ton module de connexion MySQL
 
-// with ChatGPT's help
+
 exports.getAllIngredients = async () => {
     const sql = `
         SELECT * from Ingredients
